77521812fe14a0baa49469ce7527478ded6a17dc
denisqa2h <denisqa2h@yandex.ru>
2021-09-07 23:20:01 +0300

    All tasks complete

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,22 +12,15 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
-import logging
+import logging, ephem, datetime, settings
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
 
 
 def greet_user(update, context):
@@ -35,19 +28,55 @@ def greet_user(update, context):
     print(text)
     update.message.reply_text(text)
 
+def planets_info(update, context):
+    planet_comm = update.message.text
+    planet_name = planet_comm.split()[1]
+    
+    date_today = datetime.today().strftime('%d/%m/%Y')
+
+    if planet_name == 'Mars':
+        planet = ephem.Mars(date_today)
+
+    elif planet_name == 'Mercury':
+        planet = ephem.Mercury(date_today)
+    
+    elif planet_name == 'Venus':
+        planet = ephem.Venus(date_today)
+    
+    elif planet_name == 'Jupiter':
+        planet = ephem.Jupiter(date_today)
+    
+    elif planet_name == 'Saturn':
+        planet = ephem.Saturn(date_today)
+    
+    elif planet_name == 'Uranus':
+        planet = ephem.Uranus(date_today)
+
+    elif planet_name == 'Neptune':
+        planet = ephem.Neptune(date_today)
+    
+    else:
+        update.message.reply_text(f'Ne znayu {planet_name}, ne trogai menya')
+        return
+
+    const_planet = ephem.constellation(planet)[1]
+    update.message.reply_text(const_planet)    
+
 
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planets_info))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
 
     mybot.start_polling()
     mybot.idle()
